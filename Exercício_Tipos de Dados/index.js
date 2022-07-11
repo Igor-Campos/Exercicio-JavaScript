4542ed3c122a3132fafa2cb9f889be10abbbd831
Igor Campos <igorrcfernandes@gmail.com>
2022-07-11 16:40:38 -0300

    Exercício

---
+++ Exercício_Tipos de Dados/index.js
@@ -0,0 +1,24 @@
+//Exercício
+
+/*
+    Dica rápido, frases(string) devem ser colocadas entre aspas(simples ou duplas) e números não levam aspas.
+*/
+
+/*
+    Declare uma variável contendo uma string
+    Declare uma variável contendo um número dentro de uma string
+    Declare uma variável com a sua idade
+    Declare duas variáveis, uma com seu nome e outra com seu sobrenome e some as mesmas
+    Coloque a seguinte frase em uma variável: It´s time
+    Verifique o tipo da variável que contém o seu nome
+
+*/
+
+let texto = 'Esse é um texto';
+let numString = '27';
+let nascimento = 28;
+let nome = 'Igor',
+    sobrenome = 'Campos';
+let juntando = nome + ' ' + sobrenome;
+let palavraIngles = "It's time";
+let verificar = (typeof nome);
