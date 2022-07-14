0e5fb13ce242887d8c2e9396525f9036a43a510c
Igor Campos <igorrcfernandes@gmail.com>
2022-07-14 11:44:56 -0300

    Exercício

---
+++ Exercício_Números e Operadores/index.js
@@ -0,0 +1,36 @@
+//Exercício
+
+
+/* 1)Qual o resultado da seguinte expressão?
+    let total = 10 + 5 * 2 / 2 + 20;          */
+
+    let total = 35;
+    console.log(total)
+
+/* 2)Crie duas expressões que retornem NaN    */
+
+    let expressao1 = 'Teste' / 2;
+    let expressao2 = 'Teste2' - 2;
+    console.log(expressao1, expressao2);
+
+/* 3)Somar a string '200' com o número 50 e retornar 250 */
+
+    let palavra = +'200' + 50;
+    console.log(palavra);
+
+/* 4)Incremente o número 5 e retorne o seu valor incrementado */
+
+    let x = 5;
+    ++x;
+    console.log(x);
+    
+/*    5)Como dividir o peso por 2?
+      let numero = '80';
+      let unidade = 'kg';
+      let peso = numero + unidade;
+      let pesoPorDois = peso / 2; */
+
+      let numero = +'80' / 2;
+      let unidade = 'kg';
+      let peso = numero + unidade;
+      console.log(peso);
