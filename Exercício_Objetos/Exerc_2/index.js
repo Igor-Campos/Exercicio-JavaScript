8bdff5fe5c4d59d41b89a80515ae5e62b70a358d
Igor Campos <igorrcfernandes@gmail.com>
2022-07-21 11:55:25 -0300

    Exercício

---
+++ Exercício_Objetos/Exerc_2/index.js
@@ -0,0 +1,22 @@
+/*
+    Nomeie 3 propriedades ou métodos de strings
+*/
+let nome = 'Igor';
+//fixed
+//length
+//slice
+
+/*
+    Nomeie 5 propriedades ou métodos de elementos do DOM
+*/
+let btn = document.querySelector('.btn');
+// .addEventListener
+// .appendChild
+// .id
+// .innerHTML
+// .outerHtml
+console.log(btn);
+
+/*
+    Busque na web um objeto (método) capaz de interagir com o clipboard, clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V 
+*/
